f3fc8d03544c4a0d6361bacc807188aba713c19a
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2020-03-13 12:52:05 +0300

    init: finish sount-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
+module.exports = function countCats(matrix) {
+  let nestedArray = [];
+  let count = 0;
+  for(let i = 0; i < matrix.length; i++){
+    if(matrix[i].includes("^^")){
+      count++;
+    }
+  }
   throw 'Not implemented';
-  // remove line with error and write your code here
+  return count;// remove line with error and write your code here
 };
