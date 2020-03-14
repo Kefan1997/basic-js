15dd6ca8c3250920f411e52b2319963817873fad
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2020-03-14 18:24:11 +0300

    feat: add carbon-dating & dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,24 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || members.length === 0) return false;
+
+  let arr = [];
+  let strNameDreamTeam;
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] == "string") {
+      let regexp = /[a-z]/i;
+      arr.push(members[i].match(regexp));
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (typeof arr[i][j] == "string") {
+        arr[i] = arr[i][j].toUpperCase();
+      }
+    }
+  }
+
+  arr.sort();
+  strNameDreamTeam = arr.join("");
+  return strNameDreamTeam;
 };
