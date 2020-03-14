52fc43f0d1c18e7e98ce37150dbc133fcce79129
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2020-03-14 16:38:39 +0300

    feat: fix problem with count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,11 +1,11 @@
 module.exports = function countCats(matrix) {
-  let nestedArray = [];
   let count = 0;
   for(let i = 0; i < matrix.length; i++){
-    if(matrix[i].includes("^^")){
-      count++;
-    }
+    for(let j = 0; j < matrix[i].length; j++){
+      if(matrix[i][j] == "^^"){
+        count++;
+      }  
+    } 
   }
-  throw 'Not implemented';
-  return count;// remove line with error and write your code here
+  return count;
 };
