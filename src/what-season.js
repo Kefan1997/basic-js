7f5d2a3c53cf3a43057998068d38bd64f8f57722
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2020-03-21 11:10:45 +0300

    feat: add transform-array & what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,21 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  const season = ["winter", "spring", "summer", "autumn"];
+
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+
+  if (!(date instanceof Date) || isNaN(date)) throw new Error(); /*isNaN(new Date().toString());// true*/
+
+  let month = date.getMonth();
+
+  if (month == 0 || month == 1 || month == 11) {
+    return season[0];
+  } else if (month > 1 && month < 5) {
+    return season[1];
+  } else if (month > 4 && month < 8) {
+    return season[2];
+  } else {
+    return season[3];
+  }
 };
