a884d450ab5683243f9af5a158719def69cbda50
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2020-03-22 13:49:08 +0300

    feat: add hanoi-tower & simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,32 @@
 const chainMaker = {
+  
+  result: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.result.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.result.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if(position - 1 < 0 || position - 1 >= this.getLength() || position === `${position}`){
+      this.result = [];
+      throw new Error;
+    }
+   
+      this.result.splice(position - 1, 1);
+      return this;
+    
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.result.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    const chain = this.result.join('~~');
+    this.result = [];
+    return chain;
   }
 };
 
